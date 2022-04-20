ab488adeae9a8fb03245519948fdfee60bb54f27
Гость <Guest@iMac-Mac005-8.local>
2022-04-20 16:09:51 +0900

    first commit

---
+++ Новая папка/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  petrov
+//
+//  Created by Гость on 20.04.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
